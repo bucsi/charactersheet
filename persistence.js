3ccf7feb83b60d63038bb59b248c8f11eacaf2b8
bucsi <11545252+bucsi@users.noreply.github.com>
2023-05-15 00:46:03 +0200

    ✨ load and save state with localStorage

--- persistence.js
+++ persistence.js
@@ -1,23 +1,54 @@
 const state = {};
 
-const addEventListeners = () =>
+function space(objStr) {
+    return JSON.stringify(JSON.parse(objStr), null, 2);
+}
+
+const setupPersistence = () =>
     setTimeout(() => {
-        document.querySelectorAll("input").forEach(numberInput =>
-            numberInput.addEventListener("input", e => {
-                collectData();
-                console.log(state);
-            })
-        );
+        if (!window.location.href.includes("/character/")) return;
+        state.id = window.location.href.split("/").at(-1);
+        state.localStorageKey = `characterSheet.${state.id}`;
+        addEventListeners();
+        setupLocalStorage(state);
     }, 1000);
 
-function collectData() {
+function addEventListeners() {
+    document.querySelectorAll("input").forEach(numberInput =>
+        numberInput.addEventListener("input", _e => {
+            saveData();
+            console.log(state);
+        })
+    );
+}
+
+function setupLocalStorage() {
+    const localStorageData = localStorage.getItem(state.localStorageKey);
+    if (localStorageData && confirm(`Load this dataset:\n ${space(localStorageData)}`)) {
+        Object.assign(state, JSON.parse(localStorageData));
+        setData(state);
+        return;
+    }
+    alert(`Initializing dataset:\n ${space(JSON.stringify(state))}`);
+    saveData();
+}
+
+function saveData() {
     document
         .querySelectorAll("input[type=number]")
         .forEach(number => (state[number.dataset.fieldName] = parseInt(number.value)));
     document
         .querySelectorAll("input[type=checkbox]")
         .forEach(checkbox => (state[checkbox.dataset.fieldName] = checkbox.checked));
+    localStorage.setItem(state.localStorageKey, JSON.stringify(state));
 }
 
-window.addEventListener("hashchange", addEventListeners);
-addEventListeners();
+function setData(state) {
+    document.querySelectorAll("input[type=number]").forEach(number => (number.value = state[number.dataset.fieldName]));
+    document
+        .querySelectorAll("input[type=checkbox]")
+        .forEach(checkbox => (checkbox.checked = state[checkbox.dataset.fieldName]));
+}
+
+window.addEventListener("hashchange", setupPersistence);
+setupPersistence();
